a2a47d3f374b156ec6dc9f4c7ef0baa02b1c03c6
DeveloperJihun <109073340+DeveloperJihun@users.noreply.github.com>
2025-09-28 03:38:42 +0900

    [#3]; feat: user검증 구현
    - 게시글 작성시에 email, password를 비교해서 존재하는 유저인지 확인하고 게시글을 작성

--- backend_mentoring/src/main/java/com/jihuni/backend_mentoring/service/ArticleService.java
+++ backend_mentoring/src/main/java/com/jihuni/backend_mentoring/service/ArticleService.java
@@ -4,15 +4,28 @@ import com.jihuni.backend_mentoring.entity.Article;
 import com.jihuni.backend_mentoring.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import com.jihuni.backend_mentoring.repository.UserRepository;
+import com.jihuni.backend_mentoring.entity.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Service
 @RequiredArgsConstructor
 public class ArticleService {
 
     private final ArticleRepository articleRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder; // 주입
 
     public Article createArticle(Article article) {
-        // title, content 유효성 검증
+        User user = userRepository.findByEmail(article.getEmail())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 이메일입니다."));
+
+        // 암호화된 비번 비교
+        if (!passwordEncoder.matches(article.getPassword(), user.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
         if (article.getTitle() == null || article.getTitle().trim().isEmpty()) {
             throw new IllegalArgumentException("제목은 비워둘 수 없습니다.");
         }
