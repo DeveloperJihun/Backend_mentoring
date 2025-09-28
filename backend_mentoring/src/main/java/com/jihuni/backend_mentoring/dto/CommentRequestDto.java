d72a053b16fbcb83fe3c65b35fbe210f862a1673
DeveloperJihun <109073340+DeveloperJihun@users.noreply.github.com>
2025-09-28 21:32:28 +0900

    [#7]; feat: comment DTO구조
    - comment Request/Response Dto구조 작성
    - Request에서는 @notblank를 사용하여 유효성 검증을 하였습니다.

---
+++ backend_mentoring/src/main/java/com/jihuni/backend_mentoring/dto/CommentRequestDto.java
@@ -0,0 +1,19 @@
+package com.jihuni.backend_mentoring.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CommentRequestDto {
+
+    @NotBlank(message = "이메일은 비워둘 수 없습니다.")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 비워둘 수 없습니다.")
+    private String password;
+
+    @NotBlank(message = "내용은 비워둘 수 없습니다.")
+    private String content;
+}
---
+++ backend_mentoring/src/main/java/com/jihuni/backend_mentoring/dto/CommentResponseDto.java
@@ -0,0 +1,21 @@
+package com.jihuni.backend_mentoring.dto;
+
+import com.jihuni.backend_mentoring.entity.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CommentResponseDto {
+    private Long commentId;
+    private String email;
+    private String content;
+
+    public static CommentResponseDto fromEntity(Comment comment) {
+        return new CommentResponseDto(
+                comment.getCommentId(),
+                comment.getEmail(),
+                comment.getContent()
+        );
+    }
+}
